a8125441367dfa2fe6a5b5a168baf928831cb16f
dawnight <weizhiqimail@foxmail.com>
2019-08-02 08:39:46 +0800

    :wrench: 修改配置文件和导航路由

---
+++ config.js
@@ -0,0 +1,32 @@
+export const LOGO_INFO = {
+  alt: 'your lines logo',
+  url: ''
+};
+
+export const NAV_LIST = [
+  { id: 'home', href: '/', name: '首页' },
+  { id: 'tv', href: '/tv', name: '影视中心' },
+  { id: 'post', href: '/post', name: '投稿' },
+  { id: 'message', href: '/message', name: '留言' },
+  { id: 'about', href: '/about', name: '关于' }
+];
+
+export const SEARCH_TYPE = {
+  TYPE: [
+    { id: 'movie', nameEn: 'movie', nameCn: '电影' },
+    { id: 'tv', nameEn: 'tv', nameCn: '电视剧' },
+    { id: 'carton', nameEn: 'carton', nameCn: '动漫' }
+  ],
+  AREA: [
+    { id: 'mainland', nameEn: 'mainland', nameCn: '中国大陆' },
+    { id: 'gat', nameEn: 'gat', nameCn: '港澳台' },
+    { id: 'usa', nameEn: 'usa', nameCn: '美国' },
+    { id: 'japan', nameEn: 'japan', nameCn: '日本' },
+    { id: 'korea', nameEn: 'korea', nameCn: '韩国' },
+    { id: 'other', nameEn: 'other', nameCn: '其他' }
+  ],
+  HELPER: [
+    { id: 'new', nameEn: 'new', nameCn: '最新' },
+    { id: 'like', nameEn: 'like', nameCn: '点赞最多' }
+  ]
+};
